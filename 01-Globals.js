cff3acb02c536180971748ee53eb3713c757b70f
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-08-12 22:40:27 +0530

    Initial commit

---
+++ 01-Globals.js
@@ -0,0 +1,10 @@
+// Globals --> NO WINDOW
+
+// Some important ones!!
+
+// __dirname -> path to the current directory
+// __filename -> file name
+// require() -> function to use modules (common js) (same as import)
+// module -> info about current module (file)
+// process -> info about the env where the program is being executed  !!VERY USEFUL!!
+
