51af30b92da2cb1b010ad3f3cc37bee627c0b9a3
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-08-20 22:37:05 +0530

    Learn asyn patterns (node native approach)

--- 12-Async_Patterns.js
+++ 12-Async_Patterns.js
@@ -1,19 +1,32 @@
 const {readFile , writeFile} = require('fs')
 
+const util = require('util')
 
-const getText = (path)=>{
-    return new Promise((resolve , reject)=>{
-        readFile(path, "utf8", (err, data) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(data)
-          }
-        });
-    })
+const readFIlePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+// const getText = (path)=>{
+//     return new Promise((resolve , reject)=>{
+//         readFile(path, "utf8", (err, data) => {
+//           if (err) {
+//             reject(err)
+//           } else {
+//             resolve(data)
+//           }
+//         });
+//     })
+// }
+
+// getText("./content/first.txt")
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err))
+
+const start = async()=>{
+    try {
+        const first = await readFIlePromise("./content/first.txt",'utf8');
+        console.log(first);
+        await writeFilePromise('./content/third.txt','Yo Yo Honey Singh')
+    } catch (error) {
+        console.log(error);
+    }
 }
-
-getText("./content/first.txt")
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
-
+start()
